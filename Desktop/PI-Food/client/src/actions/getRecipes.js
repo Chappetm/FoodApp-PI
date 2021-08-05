fea8feaef8d4f9b08cbdad279b3a45444c646b1d
Matias Chappet <matiaschappet@gmail.com>
2021-08-05 15:46:06 -0300

    getRecipes casi listo

--- Desktop/PI-Food/client/src/actions/getRecipes.js
+++ Desktop/PI-Food/client/src/actions/getRecipes.js
@@ -1,8 +1,22 @@
 import { GET_RECIPES } from ".";
+import axios from "axios";
 
-export default function GET_RECIPES(){
-    return {
-        type: GET_RECIPES,
-        payload
+export default function GET_RECIPES(name){
+    return async function(dispatch){
+        if(name){
+            const response = await axios.get(`http://localhost:3001/recipe/${name}`);
+            const finalResponse = response.data;
+            return dispatch({
+                type: GET_RECIPES,
+                payload: finalResponse
+            })
+        } else {
+            const response = await axios.get(`http://localhost:3001/recipe`);
+            const finalResponse = response.data;
+            return dispatch({
+                type: GET_RECIPES,
+                payload: finalResponse
+            })
+        }
     }
 }
