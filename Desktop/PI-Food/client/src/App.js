39ba636aa0d1c8e79de6357f1097f5207afe6bd8
Matias Chappet <matiaschappet@gmail.com>
2021-08-04 21:04:09 -0300

    Init Landing y Home

--- Desktop/PI-Food/client/src/App.js
+++ Desktop/PI-Food/client/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { Route } from 'react-router-dom'
+import React from 'react';
+import Landing from './components/Landing/Landing'
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
+    <React.Fragment>
+      <Route exact path='/'>
+        <Landing />
+      </Route>
+      <Route exact path='/home'>
+        <Home />
+      </Route>
+    </React.Fragment>
   );
 }
 
